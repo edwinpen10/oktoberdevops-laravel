fb086159c7eaacf1be8a5f8cdf99092df3c6f0b2
Agung <agungskripsi05@gmail.com>
2022-11-02 09:52:51 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('/version', function () {
-    return ["version" => "1.2.0"];
+    return ["version" => "1.2.1"];
 });
 
 Route::get('/lang', function () {
